9585bffcc95ec2605f668dd93aa9acad9b9b3267
vya58 <vya58@yandex.ru>
2022-11-25 23:08:01 +0300

    Поправил Readmy, добавил init.php

---
+++ init.php
@@ -0,0 +1,10 @@
+<?php
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+//Установка локали и даты
+setlocale(LC_ALL, "russian");
+
+require_once('vendor/autoload.php');
