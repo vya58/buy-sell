3708d57891b6d882f512685f7b6ae96b54b58b9f
vya58 <vya58@yandex.ru>
2022-11-26 18:05:30 +0300

    Сделал лендинг сайта

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -4,11 +4,6 @@
 /** @var string $content */
 
 use app\assets\AppAsset;
-use app\widgets\Alert;
-use yii\bootstrap5\Breadcrumbs;
-use yii\bootstrap5\Html;
-use yii\bootstrap5\Nav;
-use yii\bootstrap5\NavBar;
 
 AppAsset::register($this);
 
